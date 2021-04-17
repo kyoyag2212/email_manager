75679bc90959faa7782712f52e699e55debd82f7
Yash Sunil Lokhande <74088135+YashLokhande@users.noreply.github.com>
2021-04-17 18:23:36 +0530

    Add files via upload

--- mailhandler.js
+++ mailhandler.js
@@ -2,7 +2,12 @@ const express = require('express') // Include ExpressJS
 const app = express() // Create an ExpressJS app
 const bodyParser = require('body-parser'); // Middleware
 const  sqlite3 = require('sqlite3').verbose();// Include sqlite3
-var nodemailer = require('nodemailer');//Include nodemailer
+const nodemailer = require('nodemailer');//Include nodemailer
+
+//Logged user Data
+var Username = "";
+var PEmail = "";
+var SEmail = "";
 
 // Save Registered user data in db
 function Register (username,password,primary_email,secondary_email){
@@ -25,11 +30,19 @@ function Register (username,password,primary_email,secondary_email){
 function Authenticate (username,password){
     var pass = "";
     var db = new sqlite3.Database('emailmanager');// Connect db
-    db.run("SELECT PASS FROM user_data WHERE UID = ?",username, function(err, row) {
+    db.run("SELECT PASS, PEMAIL, SEMAIL FROM user_data WHERE UID = ?",username, function(err, row) {
+      if(err){
+        pass="";
+      }
+      else{
             pass = row.PASS;
+            PEmail = row.PEMAIL;
+            SEmail = row.SEMAIL;
+      }
         });
     db.close();// Close connection
-    if(pass = password){
+    if(pass == password){
+        Username = username;
         return true;
     }
     return false;
@@ -72,21 +85,41 @@ app.get('/login', (req, res) => {
   res.sendFile(__dirname + '/static/login.html');
 });
 
+// Route to Login Page
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/static/register.html');
+  });
+
 app.post('/login', (req, res) => {
-  // Username & Password for Validation
-  var usn = "Mango";
-  var pas = "Fruit"; 
+
   // Insert Login Code Here
   let username = req.body.username;
   let password = req.body.password;
-  if(username==usn && password==pas){
-    res.send(`Welcome ${username} !`);
+  if(Authenticate(username,password)){
+    res.sendFile(__dirname + '/static/home.html');
   }
   else{
-    res.send(`Wrong Data Entered. Username: ${username} Password: ${password}`);
+    res.sendFile(__dirname + '/static/login.html');
   }
   
 });
+
+app.post('/home', (req, res) => {
+
+    let sender = req.body.sender;
+    let pass = req.body.pass;
+    let reciever = req.body.reciever;
+    let sub = req.body.sub;
+    let msg = req.body.msg;
+
+    if(sendmail(sender,pass,reciever,sub,msg)){
+        res.send(`Mail Sent.`);
+    }
+    else{
+        res.send(`Failed to send mail`)
+    }
+    
+  });
 
 const port = 8080 // Port we will listen on
 
